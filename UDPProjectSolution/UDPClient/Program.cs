ad6f5e59e58cc8ba56c6bdaf15ecf7e47c37e71f
aurelahasanaj <162487885+aurelahasanaj@users.noreply.github.com>
2025-11-15 02:54:33 +0100

    Added client initialization and setup

--- UDPProjectSolution/UDPClient/Program.cs
+++ UDPProjectSolution/UDPClient/Program.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UDPClient
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Shkruaj IP të serverit: ");
+        string serverIP = Console.ReadLine();
+        int port = 9000;
+
+        UdpClient client = new UdpClient();
+        IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse(serverIP), port);
+
+        Console.WriteLine("Klienti u nis.");
+        Console.WriteLine("Shkruaj komanda: /list, /read, /search, /info, /delete, /upload, /download, STATS, /exit");
 ﻿
