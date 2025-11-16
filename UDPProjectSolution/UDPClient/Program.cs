7683384ca17ab322ab5be0edfc092ed525089ce2
aurelahasanaj <162487885+aurelahasanaj@users.noreply.github.com>
2025-11-16 15:39:25 +0100

    Update Program.cs

--- UDPProjectSolution/UDPClient/Program.cs
+++ UDPProjectSolution/UDPClient/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 class UDPClient
 {
@@ -10,7 +11,25 @@ class UDPClient
         Console.Write("Shkruaj IP të serverit: ");
         string serverIP = Console.ReadLine();
         int port = 9000;
+  Console.Write("A je admin? (po/jo): ");
+        bool isAdmin = Console.ReadLine().Trim().ToLower() == "po";
+ if (isAdmin)
+        {
+            Console.Write("Shkruaj fjalëkalimin e adminit: ");
+            string pass = Console.ReadLine();
 
+            if (pass != "admin123")
+            {
+                Console.WriteLine("Fjalëkalimi është gabim.");
+                return;
+            }
+
+            Console.WriteLine("Qasje ADMIN.");
+        }
+        else
+        {
+            Console.WriteLine("Qasje normale (vetëm READ).");
+        }
         UdpClient client = new UdpClient();
         IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse(serverIP), port);
 
